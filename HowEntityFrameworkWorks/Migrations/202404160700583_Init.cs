79c729b40f66e02eccc9513e3d6d96727f724e51
nikolay.milushev@fourth.com <nikolay.milushev@fourth.com>
2024-04-26 17:10:25 +0300

    HowEntityFrameworkWorks

---
+++ HowEntityFrameworkWorks/Migrations/202404160700583_Init.cs
@@ -0,0 +1,25 @@
+﻿namespace HowEntityFrameworkWorks.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ HowEntityFrameworkWorks/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace HowEntityFrameworkWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MyDb())
+            {
+                db.Products.Add(new Product { Name = "Product 1" });
+                db.Products.Add(new Product { Name = "Product 2" });
+                db.SaveChanges();
+
+                var products = db.Products.ToList();
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
+                
+                Console.WriteLine("Go to management studio and modify products' names");
+                Console.ReadLine();
+
+                products = db.Products.ToList();
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);//names not changed
+                }
+            }
+
+            using (var db = new MyDb())
+            {
+                var products = db.Products.ToList();
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);//names changed
+                }
+            }
+
+        }
+    }
+
+    public class MyDb: DbContext
+    {
+        public MyDb()
+            :base("server=.;database=products;integrated security=true;")
+        {
+            
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
